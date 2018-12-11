9a4de98b9ee2c55ffbb98768a788983d53f1bce0
NIK <kai364@qq.com>
2018-12-11 20:02:10 +0800

    引入SignalR

---
+++ Nik.MineSweeping/Hubs/MineHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Nik.MineSweeping.Hubs
+{
+    [HubName("mine")]
+    public class MineHub : Hub
+    {
+        [HubMethodName("hello")]
+        public void Hello()
+        {
+            Clients.All.hello("lalalla");
+        }
+    }
+}
